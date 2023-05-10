51241f02128ec55db3f9f89595c166833d5d6c06
Dogukan Celik <dogukancelik9@outlook.com>
2023-05-10 23:33:57 +0300

    navbar has been made sticky and transmission provided

---
+++ front-end-patikasi/bootstrap/medium-clone/js/custom.js
@@ -0,0 +1,11 @@
+const navbarDOM = document.querySelector("#sticky-navbar");
+const trendingDOM = document.querySelector("#trending");
+
+addEventListener("scroll",() => {
+    if(trendingDOM.getBoundingClientRect().y < 120) {
+        navbarDOM.style.backgroundColor = "white";
+    } else {
+        navbarDOM.style.backgroundColor = "#ffc017";
+    }
+    // console.log("trending:",trendingDOM.getBoundingClientRect().y);
+})
